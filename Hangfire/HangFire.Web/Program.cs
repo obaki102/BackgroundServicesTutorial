aec7f1459427fa17cc4d1d12f4582356c91cc7d6
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2024-09-20 13:38:28 +0800

    Added WebPuller

--- Hangfire/HangFire.Web/Program.cs
+++ Hangfire/HangFire.Web/Program.cs
@@ -1,19 +1,59 @@
+using System.Text.Json;
 using Hangfire;
+using HangFire.Web.Properties.Jobs;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.AddConsole();
-
-builder.Services.AddHangfire(config => {
+builder.Services.AddHttpClient();
+builder.Services.AddHangfire(config =>
+{
   var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
   config.UseSqlServerStorage(connectionString);
-  config.UseColouredConsoleLogProvider(); 
+  config.UseColouredConsoleLogProvider();
 });
 
 builder.Services.AddHangfireServer();
 var app = builder.Build();
 app.MapHangfireDashboard();
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/pull", (IBackgroundJobClient bg) =>
+{
+  var url = "https://consultwithgriff.com/rss.xml";
+  var directory = @"C:\repos\BackgroundServicesTutorial\rss\";
+  var filename = "consultwithgriff.json";
+  var tempPath = Path.Combine(directory, filename);
+
+  var result = bg.Enqueue<WebPuller>(p => p.GetRssItemUrlsAsync(url, tempPath));
+});
+
+app.MapGet("/sync", (IBackgroundJobClient bg) =>
+{
+  var directory = @"C:\repos\BackgroundServicesTutorial\rss";
+  var filename = "consultwithgriff.json";
+
+  var path = Path.Combine(directory, filename);
+  var json = File.ReadAllText(path);
+  var rssItemUrls = JsonSerializer.Deserialize<List<string>>(json);
+
+  var outputPath = Path.Combine(directory, "output");
+  if (!Directory.Exists(outputPath)) Directory.CreateDirectory(outputPath);
+
+  if (rssItemUrls == null || rssItemUrls.Count == 0) return;
+  var delayInSeconds = 5;
+  foreach (var url in rssItemUrls)
+  {
+    var u = new Uri(url);
+    var stub = u.Segments.Last();
+    // trim trailing slash, if any and add .html
+    if (stub.EndsWith("/")) stub = stub.Substring(0, stub.Length - 1);
+    stub += ".html";
+
+    var filePath = Path.Combine(outputPath, stub);
+
+    bg.Schedule<WebPuller>(p => p.DownloadFileFromUrl(url, filePath),
+        TimeSpan.FromSeconds(delayInSeconds));
+    delayInSeconds += 5;
+  }
+});
 
 app.Run();
- 
